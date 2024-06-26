000476b1ba2be59c8318e1cad530d931136fd15f
STEPSYS <laal@stibosystems.com>
2024-06-27 02:21:10 +0800

    Committed from STEP 2024-06-26T18:21Z

---
+++ modified-laal-vm/Primary Items/BusinessRule/BusinessRule_ClassificationsAndAssets_NodeHandler.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ClassificationsAndAssets_NodeHandler",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "ClassificationsAndAssets_NodeHandler",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
