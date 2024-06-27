87000aab28b913a0d6ddc17775bcac416095627c
STEPSYS <laal@stibosystems.com>
2024-06-28 01:27:12 +0800

    Committed from STEP 2024-06-27T17:27Z

---
+++ modified-laal-vm/Primary Items/BusinessRule/BusinessRule_DumpReferences.js
@@ -0,0 +1,39 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "DumpReferences",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "DumpReferences",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+
+node
+}
