fcb95478b9c5227ddd5f4c041a0b1aacc30fbba5
STEPSYS <laal@stibosystems.com>
2024-06-28 01:58:09 +0800

    Committed from STEP 2024-06-27T17:58Z

--- modified-laal-vm/Primary Items/BusinessRule/BusinessRule_DumpReferences.js
+++ modified-laal-vm/Primary Items/BusinessRule/BusinessRule_DumpReferences.js
@@ -35,5 +35,23 @@
 */
 exports.operation0 = function (node) {
 
-node
+
+logger.info('\ngetReferences')
+node.getReferences().asList().forEach(function (/** @type{Reference}*/ r) {
+    logger.info("aRef Type: " + r.getLinkType() + " Target: " + r.getTarget().getID() + ' sourceID=' + r.getSource().getID() + ' isDimensionPointInherited=' + r.isDimensionPointInherited())
+})
+
+logger.info('\ngetLocalReferences')
+node.getLocalReferences().asList().forEach(function (r) {
+    //logger.info(r.getClass().getName())
+    logger.info("lRef Type: " + r.getLinkType() + " Target: " + r.getTarget().getID() + ' sourceID=' + r.getSource().getID() + ' isDimensionPointInherited=' + r.isDimensionPointInherited())
+})
+
+logger.info('\nqueryReferences')
+var refType = node.getManager().getReferenceTypeHome().getReferenceTypeByID("LanguageDependantRef")
+node.queryReferences(refType).forEach(function (r) {
+    //logger.info('qType: ' + r.getClass().getName())
+    logger.info("qRef Type: " + r.getLinkType() + " Target: " + r.getTarget().getID() + ' sourceID=' + r.getSource().getID() + ' isDimensionPointInherited=' + r.isDimensionPointInherited())
+    return true
+})
 }
