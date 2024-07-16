65675e7ae40aea75e7749bd5f4758171a32e4626
STEPSYS <laal@stibosystems.com>
2024-07-17 01:50:14 +0800

    Committed from STEP 2024-07-16T17:50Z

---
+++ AutoUpdated/Primary Items/BusinessRule/BusinessRule_ItemFeed_Joiner.js
@@ -0,0 +1,66 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ItemFeed_Joiner",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "ItemFeed_Joiner",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "OutboundBusinessProcessorJoinerResultBindContract",
+    "alias" : "joinerResult",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "OutboundBusinessProcessorJoinerSourceBindContract",
+    "alias" : "joinerSource",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (joinerResult,joinerSource) {
+// Joiner Source bound joinerSource
+// Joiner Result bound to joinerResult
+
+function appendFromGroup(messageGroup) {
+  var seen = [];
+  var first = true;
+  while(joinerSource.hasNext(messageGroup)) {
+    var messageString = joinerSource.getNextMessage(messageGroup);
+    var hash = messageString.hashCode();
+    if (seen.indexOf(hash) == -1) {
+      seen.push(hash);
+      if (first) {
+        first = false;
+      } else {
+        joinerResult.appendToMessage("\n");
+      }
+      joinerResult.appendToMessage(messageString);
+    }
+  }
+}
+
+appendFromGroup("data");
+
+}
