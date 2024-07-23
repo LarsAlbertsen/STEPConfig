83f36c614a4a57f66394ab0fbacd85cd8f59ba4a
STEPSYS <laal@stibosystems.com>
2024-07-24 03:34:14 +0800

    Committed from STEP 2024-07-23T19:34Z

--- AutoUpdated/Primary Items/BusinessRule/BusinessRule_ChangePackages.js
+++ AutoUpdated/Primary Items/BusinessRule/BusinessRule_ChangePackages.js
@@ -28,11 +28,37 @@
     "parameterClass" : "null",
     "value" : null,
     "description" : null
+  }, {
+    "contract" : "AttributeBindContract",
+    "alias" : "GITBranch",
+    "parameterClass" : "com.stibo.core.domain.impl.AttributeImpl",
+    "value" : "GITBranch",
+    "description" : null
   } ],
   "messages" : [ ],
   "pluginType" : "Operation"
 }
 */
-exports.operation0 = function (manager) {
+exports.operation0 = function (manager,GITBranch) {
 
+/** @type{ChangePackageHome} */
+var cpHome = manager.getHome(com.stibo.core.domain.changepackage.ChangePackageHome)
+
+var cp = cpHome.getChangePackageByID("MyCP")
+if (cp == null) {
+    cp = cpHome.createChangePackage("Support Change Packages", "MyCP")
+    cp.setName("My Sample ChangePackage")
+    cp.setSimpleValue(GITBranch, "DEV")
+}
+else {
+    cp.reOpen()
+}
+
+var it = manager.getProductHome().getTopProduct().getChildren().iterator()
+while (it.hasNext()) {
+    cp.addItem(it.next())
+}
+
+var bgpID = cp.startSealInBackground("Auto Sealed")
+logger.info("BGPID=" + bgpID)
 }
---
+++ AutoUpdated/Primary Items/BusinessRule/BusinessRule_LanguageDependant.js
@@ -0,0 +1,39 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "LanguageDependant",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "LanguageDependant",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager) {
+lovValue = manager.getListOfValuesHome().getListOfValuesByID(lovID).getListOfValuesValueByID(valueID);
+lovValue.isLanguageInherited() --> does not work, because it is not available in the standard API 
+}
