f3ad00dbc85c7bbda67aa2a4183c77759bafe559
LAAL <laal@stibosystems.com>
2024-07-30 03:57:10 +0800

    Committed from STEP 2024-07-29T19:57Z

---
+++ Feature_4431/Primary Items/BusinessRule/BusinessRule_AutoSeal.js
@@ -0,0 +1,84 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Main"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "AutoSeal",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "TrackChanges" ],
+  "name" : "AutoSeal",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager) {
+/** @type{ChangePackageHome} */
+var cpHome = manager.getHome(com.stibo.core.domain.changepackage.ChangePackageHome)
+var cp = cpHome.getChangePackageByID("AutoUpdated")
+
+if (!isOpen(cp)) {
+	log.info("AddToChangePackage Reopen")
+	cp.reOpen()
+}
+
+removeDeletedItems(cp)
+cp.startSealInBackground("AutoSeal")
+
+
+/**
+ * 
+ * @param {ChangePackage} pCP 
+ * @returns {boolean}
+ */
+function isOpen(pCP) {
+	var state = pCP.getValue("ChangePackageState").getSimpleValue()
+	logger.info("AddToChangePackage state = " + state)
+	if ("Change Package (Sealed)".equals(state)) {
+		logger.info("isOpen(" + pCP.getName() + " false")
+		return false
+	}
+	logger.info("isOpen(" + pCP.getName() + " true")
+	return true;
+}
+
+/**
+ * 
+ * @param {ChangePackage} pCP 
+ */
+function removeDeletedItems(pCP) {
+	var allItems = pCP.getPrimaryItems();
+	allItems.toArray().forEach(function (url) {
+		if (url != null) {
+			var n = pCP.getManager().getNodeFromURL(url);
+			if (n == null) {
+				logger.info("AddToChangePackage.removeDeletedItems item in package is deleted " + url)
+				pCP.removeItem(url)
+			}
+		}
+	})
+}
+
+
+}
